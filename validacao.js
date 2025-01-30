0d364ce27f1b0d57c96ec5c4e52791c5a2a551d1
KamiMonteiro <monteiro.isabeldossantos@gmail.com>
2025-01-29 23:54:18 -0300

    mudanças no js, para poder ver o console log do input do nome. Preciso fazer a retirada do required do html para dar certo ou apenas preencher os campos.

--- validacao.js
+++ validacao.js
@@ -14,11 +14,11 @@ const form = document.getElementById("form");
 })*/
 
 
-form.addEventListener("submit", (e) => {
+/*form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const data = new FormData(form);
     for (const [name,value] of data) {
       console.log(name, ":", value)
     }
-})
+})*/
--- validacao_yt.js
+++ validacao_yt.js
@@ -1,1 +1,8 @@
-const form = document.querySelector('#form');
+const form = document.querySelector('form');
+const input = document.querySelector('input[name= "nome"]');
+
+
+form.addEventListener('submit', (ev) => {
+   ev.preventDefault();
+   console.log('75262984')
+})
